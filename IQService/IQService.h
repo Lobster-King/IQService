d9eb9f4caf9744f09d9776f37b9718e1b371fd91
Lobster-King <zhiwei.geek@gmail.com>
2019-04-28 22:14:46 +0800

    init project

---
+++ IQService/IQService.h
@@ -0,0 +1,13 @@
+//
+//  IQService.h
+//  IQService
+//
+//  Created by lobster on 2019/4/28.
+//  Copyright © 2019 lobster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IQService : NSObject
+
+@end
---
+++ IQService/IQService.m
@@ -0,0 +1,13 @@
+//
+//  IQService.m
+//  IQService
+//
+//  Created by lobster on 2019/4/28.
+//  Copyright © 2019 lobster. All rights reserved.
+//
+
+#import "IQService.h"
+
+@implementation IQService
+
+@end

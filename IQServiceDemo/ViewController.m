b851cb49fa4623bb3041867f71530bf3d771b255
Lobster-King <zhiwei.geek@gmail.com>
2019-05-11 15:24:04 +0800

    comment

--- IQServiceDemo/ViewController.m
+++ IQServiceDemo/ViewController.m
@@ -17,28 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//#warning 卧槽，神奇的崩溃，崩溃原因与函数返回值有很大的关系。
-    /*
-     1.返回值英文、无拼接->不崩溃
-     2.返回值英文、有拼接->崩溃
-     3.返回值中文、无拼接->崩溃
-     4.返回值中文、有拼接->崩溃
-     */
     
     NSString *cookie = [IQService invokeMicroServiceSync:@"GetCookieSyncService",@"我是同步调用",nil];
     NSLog(@"%@",cookie);
-    
-//    NSString *cookie = [IQService invokeMicroServiceSync:@"GetCookieSyncService",@"asd",nil];
-//    NSLog(@"%@",cookie);
-    
-//    NSString *cookie = [IQService invokeMicroServiceSync:@"GetCookieSyncService",@"asd",nil];
-//    NSLog(@"%@",cookie);
-//
-//    NSString *cookie = [IQService invokeMicroServiceSync:@"GetCookieSyncService",@"中文",nil];
-//    NSLog(@"%@",cookie);
-//
-//    NSString *cookie = [IQService invokeMicroServiceSync:@"GetCookieSyncService",@"中文",nil];
-//    NSLog(@"%@",cookie);
 
     void (^callBack)(NSString *) = ^(NSString *cookie){
         NSLog(@"%@",cookie);
